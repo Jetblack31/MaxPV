888db86e16b40280f1ab52158d74675c2f89c63f
Jetblack <28689409+Jetblack31@users.noreply.github.com>
2022-07-16 19:37:54 +0200

    Update amorceESP.ino

--- MaxPV/amorceESP/amorceESP.ino
+++ MaxPV/amorceESP/amorceESP.ino
@@ -1,50 +1,36 @@
  /*
-  AsyncElegantOTA Demo Example - This example will work for both ESP8266 & ESP32 microcontrollers.
-  -----
-  Author: Ayush Sharma ( https://github.com/ayushsharma82 )
-  
-  Important Notice: Star the repository on Github if you like the library! :)
-  Repository Link: https://github.com/ayushsharma82/AsyncElegantOTA
-*/
+  Programme d'amorce pour permettre le téléchargement des firmware et filesystem dans le Wemos 
+  */
 
-#if defined(ESP8266)
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
-#elif defined(ESP32)
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-#endif
-
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
 
 const char* ssid = "MaxPV";
 const char* password = "MaxPVMaxPV";
-IPAddress local_IP(192,168,4,1);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
 
-AsyncWebServer server(80);
+IPAddress local_IP ( 192, 168, 4, 1 );
+IPAddress gateway ( 192, 168, 4, 1 );
+IPAddress subnet ( 255, 255, 255, 0 );
 
+AsyncWebServer server ( 80 );
 
-void setup(void) {
-  Serial.begin(115200);
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  WiFi.softAP(ssid, password);
-  Serial.println("");
+void setup ( void ) {
+  Serial.begin ( 115200 );
 
-  Serial.println("");
-  //Serial.print("Connected to ");
-  //Serial.println(ssid);
+  WiFi.softAPConfig ( local_IP, gateway, subnet );
+  WiFi.softAP ( ssid, password );
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "Hi! This is a sample response.");
-  });
+  server.on ( "/", HTTP_GET, []( AsyncWebServerRequest *request ) {
+    request->send ( 200, "text/plain", "Connect to http://192.168.4.1/update" );
+  } );
 
-  AsyncElegantOTA.begin(&server);    // Start AsyncElegantOTA
-  server.begin();
-  Serial.println("HTTP server started");
+  AsyncElegantOTA.begin ( &server );    // Start AsyncElegantOTA
+
+  server.begin ( );
+  
+  Serial.println ( "HTTP server started, connect to http://192.168.4.1/update" );
 }
 
-void loop(void) {
-}
+void loop ( void ) { }
