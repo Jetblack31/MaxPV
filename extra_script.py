a48d53fce4664880b3e7be214ce5766e316eec88
Gaëtan ARS <29622411+gaetanars@users.noreply.github.com>
2022-11-20 07:54:39 +0100

    Renommage des binaires avec pio

--- extra_script.py
+++ extra_script.py
@@ -1,3 +1,29 @@
+import os
+import re
 Import("env")
-env.Replace(ESP8266_FS_IMAGE_NAME="MaxPV3_filesystem")
-env.Replace(PROGNAME="MaxPV3_firmware")
+
+SOURCECODE = "./MaxPV3/MaxPV3.ino"
+
+# Access to global construction environment
+build_tag = env['PIOENV']
+
+# Search Version
+version = ""
+file1 = open(SOURCECODE, 'r')
+Lines = file1.readlines()
+
+count = 0
+# Strips the newline character
+for line in Lines:
+    match = re.search(r"^#define\s+MAXPV_VERSION\s+.(.*).\s*$", line)
+    if match:
+        version = match.group(1)
+        file1.close
+        break
+
+if version == "":
+    raise Exception(f"No version found in {SOURCECODE}")
+
+# Rename binary according to environnement/board
+env.Replace(ESP8266_FS_IMAGE_NAME=f"MaxPV{version}_{build_tag}_filesystem")
+env.Replace(PROGNAME=f"MaxPV{version}_{build_tag}_firmware")
